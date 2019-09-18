323f4ff13c33e8f541067fdf2b9b2074faa76c61
Fernando Silva <development.ferjssilva@gmail.com>
2019-09-18 09:02:05 -0300

    text: add motivation content

--- pages/motivation.js
+++ pages/motivation.js
@@ -3,7 +3,20 @@ import Layout from '../components/layout/baseLayout';
 export default function Motivation() {
   return (
     <Layout>
-      <h1>Motivation</h1>
+      <h1>The Hackathon Guide of the galaxy</h1>
+      <h2>Motivation</h2>
+      <p>Here we will talk a litlle about why you should participate or run a hackathon.</p>
+      <p>
+        If you will to find solutions, change things or test your limits a hackathon is a great place to do this. <br/>
+        The point is that in a Hackathon we can find solutions and much more!
+      </p>
+      <p>
+        If you think about it, in a hackathon we have so many brilliant minds in the same place, willing to find solutions and to cross bounds. That's at least amazing or much more. <br/>
+        It's possible to meet those people, work together, teach, learn, have fun and, if you have some luck, make the world a litlle better.
+      </p>
+      <p>
+        Continue reading and understand why you should participate or run your own hackthon.
+      </p>
     </Layout>
   )
 }
