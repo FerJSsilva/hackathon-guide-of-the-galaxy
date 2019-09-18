6452dbd7064e13cbea6e311e7cea6165c0e0a217
Fernando Silva <development.ferjssilva@gmail.com>
2019-09-18 09:33:40 -0300

    text: newbie motivation text

--- pages/motivation.js
+++ pages/motivation.js
@@ -5,18 +5,34 @@ export default function Motivation() {
     <Layout>
       <h1>The Hackathon Guide of the galaxy</h1>
       <h2>Motivation</h2>
-      <p>Here we will talk a litlle about why you should participate or run a hackathon.</p>
-      <p>
-        If you will to find solutions, change things or test your limits a hackathon is a great place to do this. <br/>
-        The point is that in a Hackathon we can find solutions and much more!
-      </p>
-      <p>
-        If you think about it, in a hackathon we have so many brilliant minds in the same place, willing to find solutions and to cross bounds. That's at least amazing or much more. <br/>
-        It's possible to meet those people, work together, teach, learn, have fun and, if you have some luck, make the world a litlle better.
-      </p>
-      <p>
-        Continue reading and understand why you should participate or run your own hackthon.
-      </p>
+        <p>Here we will talk a litlle about why you should participate or host a hackathon.</p>
+        <p>
+          If you will to find solutions, change things or test your limits a hackathon is a great place to do this. <br/>
+          The point is that in a Hackathon we can find solutions and much more!
+        </p>
+        <p>
+          If you think about it, in a hackathon we have so many brilliant minds in the same place, willing to find solutions and to cross bounds. That's at least amazing or much more. <br/>
+          It's possible to meet those people, work together, teach, learn, have fun and, if you have some luck, make the world a litlle better.
+        </p>
+        <p>
+          Continue reading and understand why you should participate or host your own hackthon.
+        </p>
+      <h2>Newcomers Guide</h2>
+        <p>What does it take to participate a hackathon?</p>
+        <p>
+          So many experts in the same place. Am I ready for this? <br/>
+          If you are a newbie, those and many other questions might appear in your head. Just ignore Them. <br/>
+        </p>
+        <p>
+          The main requisite to participate a Hackathon is the force of will. That's something time cannot teach and money cannot buy. <br/>
+          Hackathon is not only about technology. As said before, hackathon is about finding creative solutions. <br/>
+          If there's a necessary skill you don't know, just learn it. <br/>
+          The basic is enough to start. And start is all you got do when you are not sure of something. <br/>
+        </p>
+        <p>
+          In a Hackathon you can help to find solutions, sketch ideas in a notebook, motivate your team mates, make a presentation for your team idea, code solution, organize your team and much more. <br/>
+          There are no limits in a Hackathon. After all we are here to Hack.
+        </p>
     </Layout>
   )
 }
