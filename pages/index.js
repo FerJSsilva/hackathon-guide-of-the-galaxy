da4edae1aa491cb0323e9a89743924285e62b5b2
Fernando Silva <development.ferjssilva@gmail.com>
2019-09-18 09:01:42 -0300

    text: add index content

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,19 @@ import Layout from '../components/layout/baseLayout';
 export default function Index() {
   return (
     <Layout>
-      <p>Hello Guide 2</p>
+      <h1>The Hackathon Guide of the galaxy</h1>
+      <h2>Hackathon</h2>
+      <p>To hack is to find a creative solution for a problem.</p>
+      <p>
+        To hack is to bend the rules.It's possible to hack anything,
+        from a computer program to your own life.<br/>
+        A Hackathon is an event which people come to solve problems.<br/>
+      </p>
+      <p>The main idea is to hack while having fun!</p>
+      <p>
+        This guide is going to present you all need to know about Hackathons.<br/>
+        Here you will understand how it works, why you should participate, how to organize a hackathon and much more.
+      </p>
     </Layout>
   )
 }
